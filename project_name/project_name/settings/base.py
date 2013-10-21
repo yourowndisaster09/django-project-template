d44743cb351ead0ee4e6b5a051ba84a9231250cd
ubi <yourowndisaster0@gmail.com>
2013-10-22 01:02:02 +0800

    Add htmlmin for minifying html

--- project_name/project_name/settings/base.py
+++ project_name/project_name/settings/base.py
@@ -107,6 +107,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
 )
 
 ROOT_URLCONF = '%s.urls' % SITE_NAME
